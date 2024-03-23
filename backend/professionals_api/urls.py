f83672c22805c73581fb333dc731a54d0b662353
Kumbbar <damirnasibullin8@gmail.com>
2024-03-23 11:48:15 +0500

    0.1 add occupancy logic for skud, add refresher service

--- backend/professionals_api/urls.py
+++ backend/professionals_api/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from . import settings
 
 urlpatterns = [
-    path('', include('api.urls')),
-
-]
+    path('', include('api.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
